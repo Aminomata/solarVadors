0ad5322141b6a93a8891711f82a08b7b96e7868b
Amine <amaayoufi@realytics.io>
2019-02-13 18:50:26 +0100

    Create spawner

---
+++ solarVadors/Assets/_Scripts/SpaceShip.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class SpaceShip : MonoBehaviour
+{
+    public float speed;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        transform.position = new Vector3(transform.position.x + speed * Time.deltaTime, transform.position.y, transform.position.z);
+    }
+}
---
+++ solarVadors/Assets/_Scripts/Spawner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject spaceShip;
+    public float fireRate;
+    public Transform spaceSpawn;
+
+    private float nextSpawn = 0f;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1) && Time.time > nextSpawn)
+        {
+            nextSpawn = Time.time + fireRate;
+            Instantiate(spaceShip, spaceSpawn.position, spaceSpawn.rotation);
+        }
+    }
+}
